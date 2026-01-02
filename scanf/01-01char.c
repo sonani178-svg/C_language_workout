5fd084c7e6e6064bb19b20842b96762a337dce8d
sonani178-svg <sonani178@gmail.com>
2026-01-02 10:02:41 +0530

    02-01

---
+++ scanf/01-01char.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char ch[] = "My country name is India";
+    char letter = 'D';
+    char letter1 = 'H';
+
+    printf("%s\n" , ch);
+    printf("%c\n%c" , letter , letter1);
+
+    return 0;
+
+}
