8f5bd18a6c562e9da1313f5f1313a1bb4de889ea
sonani178-svg <sonani178@gmail.com>
2026-01-03 07:59:14 +0530

    ips.c

---
+++ scanf/interest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+
+    float p=20, r=2, t=4;
+    scanf("%f%f%f", p, r, t);
+
+    printf("SI=%f", p*r*t/100);
+
+    return 0;
+}
---
+++ scanf/positive.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+
+    int m;
+    scanf("%d", m);
+    if(m>=0)
+     printf("positive");
+else 
+    printf("negative");
+
+    return 0;
+}
